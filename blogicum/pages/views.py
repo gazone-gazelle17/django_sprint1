d98ae7d40b8451818d0e8f9f03764c504314fceb
Александр Гасымов <gazykh@yandex.ru>
2023-05-29 00:11:48 +0300

    Сдача проекта на первое ревью

---
+++ blogicum/pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
+
+# Create your views here.
